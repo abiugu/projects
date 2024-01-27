1827f98464c594077e4209e8242193d179418f41
John <abiugu@gmail.com>
2024-01-27 11:22:40 -0300

    init

--- scraping.py
+++ scraping.py
@@ -3,8 +3,25 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-service = service()
+service = Service()
 
 options = webdriver.ChromeOptions()
 
 driver = webdriver.Chrome(service=service, options=options)
+
+url = 'https://blaze-7.com/pt/games/double?modal=double_history_index'
+driver.get(url)
+
+# Aguarda até que a classe history__double seja carregada
+driver.implicitly_wait(10)
+
+# Extrai informações da classe history__double
+history_double_element = driver.find_element(By.CLASS_NAME, "history__double")
+history_text = history_double_element.text
+
+# Imprime as informações
+print(history_text)
+
+# Salva as informações em um arquivo de texto
+with open("history_output.txt", "w", encoding="utf-8") as file:
+    file.write(history_text)
