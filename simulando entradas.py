a2ada43b92cee70a44265b11dc844623969d42d4
John <abiugu@gmail.com>
2024-02-05 22:18:35 -0300

    i

--- simulando entradas.py
+++ simulando entradas.py
@@ -14,13 +14,12 @@ def somar_resultados(acertos, erros, sequencia):
     cores_anteriores = sequencia[1:4]
     cor_atual = sequencia[0]
 
-    if len(sequencia) >= 4:
-        if all(cor == cores_anteriores[0] for cor in cores_anteriores) and cor_atual == cores_anteriores[0]:
-            erros += 1
-            print("Erro!")
-        elif all(cor == cores_anteriores[0] for cor in cores_anteriores) and cor_atual != cores_anteriores[0]:
-            acertos += 1
-            print("Acerto!")
+    if all(cor == cores_anteriores[0] for cor in cores_anteriores) and cor_atual != cores_anteriores[0]:
+        acertos += 1
+        print("Acerto!")
+    elif all(cor == cores_anteriores[0] for cor in cores_anteriores) and cor_atual == cores_anteriores[0]:
+        erros += 1
+        print("Erro!")
 
     return acertos, erros
 
@@ -34,12 +33,11 @@ def main():
     options.add_experimental_option("prefs", prefs)
 
     desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
-    txt_file_path = os.path.join(desktop_path, "resultados_recentes.txt")
     resultados_path = os.path.join(desktop_path, "acertos_erros.txt")
 
     acertos, erros = 0, 0
-    resultados_anteriores = {"acertos": 0,
-                             "erros": 0, "ultimas_tres_linhas": []}
+    acertos_anterior, erros_anterior = 0, 0
+    intervalo_contagem = 15
 
     try:
         driver = webdriver.Chrome(service=service, options=options)
@@ -52,32 +50,28 @@ def main():
             box_elements = recent_results_element.find_elements(
                 By.CLASS_NAME, "sm-box")
 
-            sequencia = []
-            for i, box_element in enumerate(box_elements[:4]):
-                color = box_element.get_attribute("class").split()[-1]
-                sequencia.append(color)
+            sequencia = [box_element.get_attribute(
+                "class").split()[-1] for box_element in box_elements[:4]]
 
             acertos, erros = somar_resultados(acertos, erros, sequencia)
 
-            # Escrever resultados no arquivo
-            with open(resultados_path, "w") as file:
-                file.write(f"Acertos: {acertos}\nErros: {erros}\n")
-                file.write("Últimas 4 linhas:\n")
-                for linha in resultados_anteriores['ultimas_tres_linhas']:
-                    file.write(f"{linha}\n")
+            # Escrever resultados no arquivo a cada 60 segundos ou 15 segundos
+            if acertos + erros > acertos_anterior + erros_anterior:
+                with open(resultados_path, "w") as file:
+                    file.write(f"Acertos: {acertos}\nErros: {erros}\n")
+                    file.write("Últimas 4 linhas:\n")
+                    file.write("\n".join(sequencia) + "\n")
 
-            resultados_anteriores['acertos'] = acertos
-            resultados_anteriores['erros'] = erros
-            resultados_anteriores['ultimas_tres_linhas'] = sequencia
+                # Atualiza o número de acertos e erros do loop anterior
+                acertos_anterior, erros_anterior = acertos, erros
 
-            # Definir o intervalo com base em acerto ou erro
-            if acertos > resultados_anteriores['acertos'] or erros > resultados_anteriores['erros']:
-                intervalo = 60  # Intervalo de 60 segundos em caso de acerto ou erro
+                # Reinicia o contador de contagem para o próximo intervalo
+                intervalo_contagem = 60
             else:
-                intervalo = 20  # Intervalo de 20 segundos em caso de nenhum acerto ou erro
+                # Decrementa o contador de contagem para o próximo loop
+                intervalo_contagem = max(15, intervalo_contagem - 15)
 
-            print(f"Aguardando {intervalo} segundos...")
-            time.sleep(intervalo)
+            time.sleep(intervalo_contagem)
 
     except Exception as e:
         print(f"Erro: {e}")
