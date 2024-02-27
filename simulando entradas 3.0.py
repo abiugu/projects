3ddfe8fd0d8ab00548608ceb78d8dcb5053ee4aa
John <abiugu@gmail.com>
2024-02-27 19:51:57 -0300

    i

--- simulando entradas 3.0.py
+++ simulando entradas 3.0.py
@@ -45,15 +45,17 @@ def somar_resultados(acertos, erros, sequencia):
             print(log_text)
             erros_anterior = 0
             intervalo_contagem = 60
-            log_result = extrair_cores_25_50()  # Extrair cores após acerto martingale
-            return acertos, erros, intervalo_contagem, log_text, log_result
+            log_result_double = resultados_vistos_ultimas_25_rodadas()  # Extrair cores após acerto martingale
+            log_result_percentagens = percentual_ultimas_25_rodadas()
+            return acertos, erros, intervalo_contagem, log_text, log_result_double, log_result_percentagens
 
         else:
             log_text = f"Acerto !! Cor atual: {cor_atual}"
             print(log_text)
             intervalo_contagem = 60
-            log_result = extrair_cores_25_50()  # Extrair cores após acerto
-            return acertos, erros, intervalo_contagem, log_text, log_result
+            log_result_double = resultados_vistos_ultimas_25_rodadas()  # Extrair cores após acerto
+            log_result_percentagens = percentual_ultimas_25_rodadas()
+            return acertos, erros, intervalo_contagem, log_text, log_result_double, log_result_percentagens
 
     elif all(cor == cores_anteriores[0] for cor in cores_anteriores) and cor_atual == cores_anteriores[0]:
         erros_anterior += 1
@@ -61,8 +63,8 @@ def somar_resultados(acertos, erros, sequencia):
             log_text = f"Erro !! Cor atual: {cor_atual}"
             print(log_text)
             intervalo_contagem = 25
-            log_result = extrair_cores_25_50() # Extrair cores após erro
-            return acertos, erros, intervalo_contagem, log_text, ""
+            log_result_percentagens = percentual_ultimas_25_rodadas()
+            return acertos, erros, intervalo_contagem, log_text, "", log_result_percentagens
 
         elif erros_anterior == 2:
             log_text = f"Erro no Martingale !! Cor atual: {cor_atual}"
@@ -70,58 +72,44 @@ def somar_resultados(acertos, erros, sequencia):
             erros_anterior = 0
             erros += 3
             intervalo_contagem = 60
-            log_result = extrair_cores_25_50()  # Extrair cores após erro martingale
-            return acertos, erros, intervalo_contagem, log_text, log_result
+            log_result_double = resultados_vistos_ultimas_25_rodadas()  # Extrair cores após erro martingale
+            log_result_percentagens = percentual_ultimas_25_rodadas()
+            return acertos, erros, intervalo_contagem, log_text, log_result_double, log_result_percentagens
 
-        return acertos, erros, 25, "", ""
+        return acertos, erros, 25, "", "", ""
 
 
-def extrair_cores_25_50():
+def percentual_ultimas_25_rodadas():
     global driver
 
     if driver.current_url != "https://blaze1.space/pt/games/double?modal=double_history_index":
-        driver.get(
-            "https://blaze1.space/pt/games/double?modal=double_history_index")
-        tabs_div = WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, "tabs-crash-analytics")))
-        padroes_button = tabs_div.find_element(
-            By.XPATH, ".//button[text()='Padrões']")
-        padroes_button.click()
-        padroes_active_button = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//button[@class='tab active']")))
+        driver.get("https://blaze1.space/pt/games/double?modal=double_history_index")
 
-    select_element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//select[@tabindex='0']")))
+    select_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//select[@tabindex='0']")))
     select = Select(select_element)
 
     time.sleep(2)
-
     select.select_by_value("50")
-
     time.sleep(2)
-
     select.select_by_value("25")
-
     time.sleep(2)
 
-    text_elements_present = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.TAG_NAME, "text")))
-    text_elements_visible = WebDriverWait(driver, 10).until(
-        EC.visibility_of_all_elements_located((By.TAG_NAME, "text")))
-    valores_25 = [element.get_attribute("textContent") for element in text_elements_present
-                  if element.get_attribute("y") == "288" and "SofiaPro" in element.get_attribute("font-family")]
+    text_elements_present = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, "text")))
+    valores_25 = [element.get_attribute("textContent") for element in text_elements_present if element.get_attribute("y") == "288" and "SofiaPro" in element.get_attribute("font-family")]
 
-    log_result = "Ultimas 25 rodadas:" + \
-        str(valores_25)
+    log_result = "Ultimas 25 rodadas:" + str(valores_25)
     print(log_result)
+    return log_result
 
-    # Encontrar o primeiro elemento div com a classe "roll"
+
+def resultados_vistos_ultimas_25_rodadas():
+    global driver
+
     roll_div = driver.find_element(By.CLASS_NAME, "roll")
     roll_containers = roll_div.find_elements(By.CLASS_NAME, "roll__container")
     cores_numeros = []
     for container in roll_containers:
-        cor_class = container.find_element(
-            By.CLASS_NAME, "roll__square").get_attribute("class")
+        cor_class = container.find_element(By.CLASS_NAME, "roll__square").get_attribute("class")
         if "roll__square--red" in cor_class:
             cor = "red"
         elif "roll__square--black" in cor_class:
@@ -131,8 +119,7 @@ def extrair_cores_25_50():
 
         numero_str = container.find_element(By.TAG_NAME, "span").text
         # Remove os caracteres '<' e '>' antes de converter para inteiro
-        numero_str_limpo = ''.join(
-            filter(lambda x: x.isdigit() or x == '.', numero_str))
+        numero_str_limpo = ''.join(filter(lambda x: x.isdigit() or x == '.', numero_str))
         numero_float = float(numero_str_limpo)
         numero_int = int(numero_float)
 
@@ -163,22 +150,16 @@ def main():
         driver.get(url)
 
         while not verificar_stop():
-            recent_results_element = driver.find_element(
-                By.ID, "roulette-recent")
-            box_elements = recent_results_element.find_elements(
-                By.CLASS_NAME, "sm-box")
+            recent_results_element = driver.find_element(By.ID, "roulette-recent")
+            box_elements = recent_results_element.find_elements(By.CLASS_NAME, "sm-box")
 
-            sequencia = [box_element.get_attribute(
-                "class").split()[-1] for box_element in box_elements[:5]]
+            sequencia = [box_element.get_attribute("class").split()[-1] for box_element in box_elements[:5]]
 
-            acertos, erros, intervalo_contagem, log_text, log_result = somar_resultados(
-                acertos, erros, sequencia)
+            acertos, erros, intervalo_contagem, log_text, log_result_double, log_result_percentagens = somar_resultados(acertos, erros, sequencia)
 
             with open(acertos_erros_path, "w") as acertos_erros_file:
-                acertos_erros_file.write(
-                    f"Acertos: {acertos}\nErros: {erros}\n")
-                acertos_erros_file.write(f"Segundos do loop atual: {
-                                         intervalo_contagem}\n")
+                acertos_erros_file.write(f"Acertos: {acertos}\nErros: {erros}\n")
+                acertos_erros_file.write(f"Segundos do loop atual: {intervalo_contagem}\n")
                 acertos_erros_file.write("Últimas 5 linhas:\n")
                 acertos_erros_file.write("\n".join(sequencia) + "\n")
 
@@ -186,13 +167,13 @@ def main():
                 if log_text:
                     log_file.write(log_text + "\n")
 
-                if log_result:
-                    log_file.write(log_result + "\n")  # Ajuste aqui
-                    # Ajuste aqui
-                    log_file.write("\n".join(map(str, log_result[1])) + "\n")
+                if log_result_double:
+                    log_file.write(log_result_double + "\n")
 
-                log_file.write(f"Segundos do loop atual: {
-                               intervalo_contagem}" + "\n")
+                if log_result_percentagens:
+                    log_file.write(log_result_percentagens + "\n")
+
+                log_file.write(f"Segundos do loop atual: {intervalo_contagem}\n")
 
             print(f"Segundos do loop atual: {intervalo_contagem}")
 
