a4d060ec6323528d9a613de2996676cb4cd8f499
John <abiugu@gmail.com>
2024-04-22 23:26:21 -0300

    i

--- Automação de entradas/extração lista erros.py
+++ Automação de entradas/extração lista erros.py
@@ -1,19 +1,32 @@
 import os
 
-# Função para comparar a cor atual com a cor oposta
+# Função para comparar as porcentagens entre as cores
 def comparar_porcentagens(cor_atual, cor_oposta, porcentagens_atual, porcentagens_oposta):
     if cor_atual == "red":
-        if porcentagens_atual > porcentagens_oposta:
+        red_atual = porcentagens_atual[2]
+        red_oposta = porcentagens_oposta[1]  # Correção aqui
+        if red_atual > red_oposta:
             return ">"
-        elif porcentagens_atual < porcentagens_oposta:
+        elif red_atual < red_oposta:
             return "<"
         else:
             return "="
     elif cor_atual == "black":
-        if porcentagens_atual > porcentagens_oposta:
-            return "<"
-        elif porcentagens_atual < porcentagens_oposta:
+        black_atual = porcentagens_atual[1]
+        black_oposta = porcentagens_oposta[2]
+        if black_atual > black_oposta:
             return ">"
+        elif black_atual < black_oposta:
+            return "<"
+        else:
+            return "="
+    else:  # Cor atual é white
+        white_atual = porcentagens_atual[0]
+        white_oposta = porcentagens_oposta[2]
+        if white_atual > white_oposta:
+            return ">"
+        elif white_atual < white_oposta:
+            return "<"
         else:
             return "="
 
@@ -60,24 +73,35 @@ alarmes, black_count, red_count = identificar_alarmes(arquivo_entrada)
 
 # Função para imprimir os resultados e contar as ocorrências
 def imprimir_resultados(alarmes, black_count, red_count):
-    contagem = {"<": {"25": 0, "50": 0, "100": 0, "500": 0},
-                "=": {"25": 0, "50": 0, "100": 0, "500": 0},
-                ">": {"25": 0, "50": 0, "100": 0, "500": 0}}
+    contagem = {}
     with open(arquivo_saida, 'w') as file:
         for info in alarmes:
             cor_atual = info["cor"]
             cor_oposta = "red" if cor_atual == "black" else "black"
+            sequencia = ""
+            file.write("Ultimos 3 resultados: {}, {}, {}\n".format(cor_atual, cor_atual, cor_atual))
+            file.write("Ultimas 25 porcentagens: {}\n".format(", ".join(map(str, info["comp_25"]))))
+            file.write("Ultimas 50 porcentagens: {}\n".format(", ".join(map(str, info["comp_50"]))))
+            file.write("Ultimas 100 porcentagens: {}\n".format(", ".join(map(str, info["comp_100"]))))
+            file.write("Ultimas 500 porcentagens: {}\n".format(", ".join(map(str, info["comp_500"]))))
             for rodadas in ["25", "50", "100", "500"]:
-                comp = comparar_porcentagens(cor_atual, cor_oposta, info.get("comp_" + rodadas, [0, 0, 0])[1], info.get("comp_" + rodadas, [0, 0, 0])[2])
-                contagem[comp][rodadas] += 1
+                comp = comparar_porcentagens(cor_atual, cor_oposta, info.get("comp_" + rodadas, [0, 0, 0]), info.get("comp_" + rodadas, [0, 0, 0]))
+                sequencia += comp
                 file.write("Percentual {} rodadas: {}\n".format(rodadas, comp))
-            file.write("Cor: {}\nAlarme: {}\n\n".format(cor_atual, info["alarme"]))
+            if sequencia in contagem:
+                contagem[sequencia] += 1
+            else:
+                contagem[sequencia] = 1
+        
         file.write("\nContagem:\n")
         file.write("Black: {}\n".format(black_count))
         file.write("Red: {}\n".format(red_count))
-        for comparacao, valores in contagem.items():
-            for rodadas, quantidade in valores.items():
-                file.write("Percentual {} rodadas {}: {}\n".format(rodadas, comparacao, quantidade))
+        
+        file.write("\nPossibilidades:\n")
+        for seq, count in sorted(contagem.items(), key=lambda x: x[1], reverse=True):
+            file.write("Sequencia: {}\n".format(seq))
+            file.write("Quantidade: {}\n".format(count))
+            file.write("\n")
 
 # Imprimir resultados
 imprimir_resultados(alarmes, black_count, red_count)
