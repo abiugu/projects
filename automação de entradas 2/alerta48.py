302357bf6b3a3d755a46c668b69e5e660469a4c7
John <abiugu@gmail.com>
2024-05-10 10:26:42 -0300

    i

--- automação de entradas 2/alerta48.py
+++ automação de entradas 2/alerta48.py
@@ -36,7 +36,7 @@ desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
 logs_path = os.path.join(desktop_path, "LOGS")
 
 # Caminho completo para o arquivo de log
-log_file_path = os.path.join(logs_path, "log 48 estrategico.txt")
+log_file_path = os.path.join(logs_path, "log 48.txt")
 
 # Inicializa o mixer de áudio do pygame
 pygame.mixer.init()
@@ -48,7 +48,7 @@ sound_file_path = "ENTRADA CONFIRMADA.mp3"
 alarm_sound = pygame.mixer.Sound(sound_file_path)
 
 # Arquivo de log interativo
-log_interativo_path = os.path.join(logs_path, "resultados 48 estrategico.txt")
+log_interativo_path = os.path.join(logs_path, "resultados 48.txt")
 
 # Dicionário para armazenar valores anteriores
 valores_anteriores = {"acertos_direto": 0, "acertos_gale": 0, "erros": 0}
