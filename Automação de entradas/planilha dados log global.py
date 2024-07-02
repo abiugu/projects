6b652b9877e595fe3a073ee4e6f1d848376577ff
John <abiugu@gmail.com>
2024-07-02 17:51:03 -0300

    i

--- Automação de entradas/planilha dados log global.py
+++ Automação de entradas/planilha dados log global.py
@@ -33,16 +33,24 @@ def analisar_padroes(resultados, porcentagens_25, porcentagens_50, porcentagens_
     padroes_analise = defaultdict(lambda: {'acertos': 0, 'erros': 0, 'jogadas': 0, 'erros_consecutivos': 0, 'max_erros_consecutivos': 0})
 
     for i in range(len(resultados) - 1):
+        cor_atual = resultados[i][0]
         if resultados[i][0] == resultados[i][1] == resultados[i][2]:
+            if cor_atual == "black":
+                cor_oposta = "red"
+            elif cor_atual == "red":
+                cor_oposta = "black"
+            else:
+                continue
+
             try:
-                porcentagem_atual_25 = float(porcentagens_25[i][2])
-                porcentagem_oposta_25 = float(porcentagens_25[i][1])
-                porcentagem_atual_50 = float(porcentagens_50[i][2])
-                porcentagem_oposta_50 = float(porcentagens_50[i][1])
-                porcentagem_atual_100 = float(porcentagens_100[i][2])
-                porcentagem_oposta_100 = float(porcentagens_100[i][1])
-                porcentagem_atual_500 = float(porcentagens_500[i][2])
-                porcentagem_oposta_500 = float(porcentagens_500[i][1])
+                porcentagem_atual_25 = float(porcentagens_25[i][2] if cor_atual == "red" else porcentagens_25[i][1])
+                porcentagem_oposta_25 = float(porcentagens_25[i][1] if cor_atual == "red" else porcentagens_25[i][2])
+                porcentagem_atual_50 = float(porcentagens_50[i][2] if cor_atual == "red" else porcentagens_50[i][1])
+                porcentagem_oposta_50 = float(porcentagens_50[i][1] if cor_atual == "red" else porcentagens_50[i][2])
+                porcentagem_atual_100 = float(porcentagens_100[i][2] if cor_atual == "red" else porcentagens_100[i][1])
+                porcentagem_oposta_100 = float(porcentagens_100[i][1] if cor_atual == "red" else porcentagens_100[i][2])
+                porcentagem_atual_500 = float(porcentagens_500[i][2] if cor_atual == "red" else porcentagens_500[i][1])
+                porcentagem_oposta_500 = float(porcentagens_500[i][1] if cor_atual == "red" else porcentagens_500[i][2])
             except IndexError:
                 continue
 
@@ -52,7 +60,7 @@ def analisar_padroes(resultados, porcentagens_25, porcentagens_50, porcentagens_
             comparacao_500 = comparar_percentuais(porcentagem_atual_500, porcentagem_oposta_500)
 
             chave_padrao = (porcentagem_atual_25, comparacao_25, comparacao_50, comparacao_100, comparacao_500)
-            acerto = resultados[i + 1][0] != resultados[i][0]
+            acerto = resultados[i + 1][0] != cor_atual
 
             padroes_analise[chave_padrao]['jogadas'] += 1
 
@@ -82,11 +90,11 @@ def gerar_planilha_excel(padroes_analise, caminho_arquivo_excel):
     sheet = workbook.active
     sheet.title = 'Análise de Comparações'
 
-    header = ["Percentual Atual 25", "Comparação 25", "Comparação 50", "Comparação 100", "Comparação 500", "Acertos", "Erros", "Jogadas", "Últimos Erros Consecutivos", "Máx. Erros Consecutivos", "Assertividade Acertos (%)"]
+    header = ["Percentual Atual", "Comparação 25", "Comparação 50", "Comparação 100", "Comparação 500", "Acertos", "Erros", "Jogadas", "Últimos Erros Consecutivos", "Máx. Erros Consecutivos", "Assertividade Acertos (%)"]
     sheet.append(header)
 
     for chave_padrao, dados in padroes_analise.items():
-        percentual_atual_25, comp_25, comp_50, comp_100, comp_500 = chave_padrao
+        percentual_atual, comp_25, comp_50, comp_100, comp_500 = chave_padrao
         acertos = dados['acertos']
         erros = dados['erros']
         jogadas = dados['jogadas']
@@ -94,7 +102,7 @@ def gerar_planilha_excel(padroes_analise, caminho_arquivo_excel):
         max_erros_consecutivos = dados['max_erros_consecutivos']
         assertividade_acertos = dados['assertividade_acertos']
 
-        row = [percentual_atual_25, comp_25, comp_50, comp_100, comp_500, acertos, erros, jogadas, erros_consecutivos, max_erros_consecutivos, f"{assertividade_acertos:.2f}%"]
+        row = [percentual_atual, comp_25, comp_50, comp_100, comp_500, acertos, erros, jogadas, erros_consecutivos, max_erros_consecutivos, f"{assertividade_acertos:.2f}%"]
         sheet.append(row)
 
     workbook.save(caminho_arquivo_excel)
